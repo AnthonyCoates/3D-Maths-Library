1bea59c697b9fa207296e54114971129ec968fe3
Anthony Coates <anthjcoates@gmail.com>
2022-01-30 16:38:02 +0000

    Added standard operator overloads for Vector3 and float parameters

--- 3DMathsLibrary/3DMathsLibrary/3DMathsLibrary.cpp
+++ 3DMathsLibrary/3DMathsLibrary/3DMathsLibrary.cpp
@@ -8,6 +8,7 @@ struct Vector3
 	float y{};
 	float z{};
 
+	// Constructors
 	Vector3()
 	{
 		x = 0.0f;
@@ -20,5 +21,47 @@ struct Vector3
 		this->x = x;
 		this->y = y;
 		this->z = z;
+	}
+
+	// Operator Overloads - Vector3
+	Vector3 operator+(const Vector3& v)
+	{
+		return Vector3(x + v.x, y + v.y, z + v.z);
+	}
+
+	Vector3 operator-(const Vector3& v)
+	{
+		return Vector3(x - v.x, y - v.y, z - v.z);
+	}
+
+	Vector3 operator*(const Vector3& v)
+	{
+		return Vector3(x * v.x, y * v.y, z * v.z);
+	}
+
+	Vector3 operator/(const Vector3& v)
+	{
+		return Vector3(x / v.x, y / v.y, z / v.z);
+	}
+
+	// Operator Overloads - Float
+	Vector3 operator+(const float& f)
+	{
+		return Vector3(x + f, y + f, z + f);
+	}
+
+	Vector3 operator-(const float& f)
+	{
+		return Vector3(x - f, y - f, z - f);
+	}
+
+	Vector3 operator*(const float& f)
+	{
+		return Vector3(x * f, y * f, z * f);
+	}
+
+	Vector3 operator/(const float& f)
+	{
+		return Vector3(x / f, y / f, z / f);
 	}
 };
